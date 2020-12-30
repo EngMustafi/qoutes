2331f46a883527e3fbf46326c289ca26dfe5cde3
mustafaabdulahi879 <mustafaabdulaahi@gmail.com>
2020-12-30 12:06:14 +0300

    new commit

---
+++ form.js
@@ -0,0 +1,25 @@
+let username = document.querySelector('#username')
+let password = document.getElementById('password')
+let button = document.getElementById('btn')
+let check = document.getElementById('checking')
+
+user = 'cabaas'
+pass = 12345
+
+button.addEventListener('click',function(e){
+   if (username.value == user && password.value == pass){
+        console.log('Pass')
+
+   }
+   else if (username.value == '' || password.value == ''){
+    check.textContent='Please Enter Username and password'
+    e.preventDefault()
+}
+   else{
+        check.textContent='Incorrect Username or password'
+        e.preventDefault()
+   }
+ 
+    
+    
+})
