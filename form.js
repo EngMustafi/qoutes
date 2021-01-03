26fd3c4f7eb0a5148c99c014418cf952de7b5f27
mustafaabdulahi879 <mustafaabdulaahi@gmail.com>
2021-01-03 09:58:27 +0300

    mew commit

--- form.js
+++ form.js
@@ -3,23 +3,20 @@ let password = document.getElementById('password')
 let button = document.getElementById('btn')
 let check = document.getElementById('checking')
 
-user = 'cabaas'
-pass = 12345
+let user = 'cabaas'
+let pass = 12345
 
-button.addEventListener('submit',function(e){
-   if (username.value == user && password.value == pass){
-        console.log('Pass')
-
-   }
-   else if (username.value == '' || password.value == ''){
-    check.textContent='Please Enter Username and password'
-    e.preventDefault()
-}
-   else{
+button.addEventListener('click',function(e){
+    if (username.value == '' || password.value == ''){
+     check.textContent='Please Enter Username and password'
+     e.preventDefault()
+    }
+    else if (username.value != user && password.value != pass){
         check.textContent='Incorrect Username or password'
-        e.preventDefault()
-   }
+     e.preventDefault()
  
-    
-    
-})
+    }
+  
+     
+     
+ })
